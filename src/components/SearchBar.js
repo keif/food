58221cac4f645100ed6dbda315dd0a195be1b94e
Keith T. Baker <hello@keif.name>
2020-07-21 23:57:10 -0400

    Displaying icons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
     return (
         <View style={styles.view}>
+            <Feather name="search" size={30} color="black" />
             <Text>Search Bar</Text>
         </View>
     )
@@ -15,7 +17,7 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         height: 50,
         marginHorizontal: 15,
-    }
+    },
 })
 
 export default SearchBar
