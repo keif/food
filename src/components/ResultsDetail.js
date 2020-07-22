6b2903e74d96f116fe964dce300edadf7235e032
Keith T. Baker <hello@keif.name>
2020-07-22 15:12:15 -0400

    showing additional info

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,14 +1,30 @@
 import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
+import {Image, StyleSheet, Text, View} from 'react-native'
 
 const ResultsDetail = ({result}) => {
     return (
         <View>
-            <Text>{result.name}</Text>
+            <Image
+                source={{ uri: result.image_url }}
+                style={styles.image}
+            />
+            <Text
+                style={styles.name}
+            >{result.name}</Text>
+            <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image: {
+        borderRadius: 4,
+        height: 120,
+        width: 250,
+    },
+    name: {
+        fontWeight: `bold`,
+    },
+})
 
 export default ResultsDetail
