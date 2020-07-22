25b24a2c90a2eaf1525c889e71a8c27953df4c86
Keith T. Baker <hello@keif.name>
2020-07-22 16:44:45 -0400

    fetching a single restaraunt

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,8 +1,26 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import {StyleSheet, Text, View} from "react-native"
+import yelp from "../api/yelp"
 
 const ResultsShowScreen = ({navigation}) => {
-    const id =  navigation.getParam(`id`)
+    const [result, setResult] = useState(null)
+    const id = navigation.getParam(`id`)
+
+    console.log(`id: ${id}`)
+    console.log(`result:`, result)
+
+    const getResult = async (id) => {
+        try {
+            const response = await yelp.get(`/${id}`)
+            setResult(response.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() =>{
+        getResult(id)
+    }, [])
 
     return (
         <View>
@@ -12,5 +30,5 @@ const ResultsShowScreen = ({navigation}) => {
 }
 
 const styles = StyleSheet.create({})
- 
+
 export default ResultsShowScreen
